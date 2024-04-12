a85087bb1e68e0c1b82938343501419e6feed6d7
gorguero <jcaguerodev@gmail.com>
2024-04-12 16:34:19 -0300

    Variables de entorno

--- rest-server/src/presentation/server.ts
+++ rest-server/src/presentation/server.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import path from 'path';
 
-
+interface Options{
+    port: number;
+    public_path?: string;
+}
 
 
 export class Server{
 
     private app = express();
 
-    constructor(){}
+    private readonly port:number;
+    private readonly public_path:string;
+
+    constructor( options:Options ){
+        const { port, public_path = 'public' } = options;
+        this.port = port;
+        this.public_path = public_path;
+    }
 
 
     async start(){
@@ -17,16 +27,16 @@ export class Server{
 
 
     /* PUBLIC FOLDER */
-    this.app.use( express.static('public') );
+    this.app.use( express.static( this.public_path ) );
 
     /* SPA */
     this.app.get('*', (req, res) => {
-        const indexPath = path.join(__dirname + '../../public/index.html');
+        const indexPath = path.join(__dirname + `../../${ this.public_path }/index.html`);
         res.sendFile(indexPath);
-    })
+    });
 
-    this.app.listen(3000, () => {
-        console.log(`Server running on port ${3000}`);
+    this.app.listen(this.port, () => {
+        console.log(`Server running on port ${this.port}`);
     });
 
     }
